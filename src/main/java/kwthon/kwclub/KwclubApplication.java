5d6def667dc06e9845008585251a302d98a2b0e6
dohyun0313 <parkdohyun0313@naver.com>
2024-11-14 22:50:26 +0900

    1

---
+++ src/main/java/kwthon/kwclub/KwclubApplication.java
@@ -0,0 +1,13 @@
+package kwthon.kwclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwclubApplication.class, args);
+	}
+
+}
