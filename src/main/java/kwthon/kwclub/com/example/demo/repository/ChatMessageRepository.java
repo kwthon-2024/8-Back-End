2fda40e066aa5d670a6c6de9ed0f494fc6d91f98
zzq3902 <zzq3902@naver.com>
2024-11-15 21:51:18 +0900

    t p#

--- src/main/java/kwthon/kwclub/com/example/demo/repository/ChatMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package kwthon.kwclub.com.example.demo.repository;
-
-import kwthon.kwclub.com.example.demo.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    
-}
